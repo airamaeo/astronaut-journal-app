ce6b0483afd3637fafd5cb8af2c2c6f40b8197ea
Aira Mae Ogaco <airamaeogaco@gmail.com>
2025-06-26 11:56:02 +0100

    Implemented APOD fetch with functional timeline and photo detail views. Added navigation with back buttons for improved user flow

--- frontend/src/pages/PhotoDetail.jsx
+++ frontend/src/pages/PhotoDetail.jsx
@@ -1,7 +1,51 @@
 // Reads :year from URL param
 // Fetches single APOD from backend /api/nasa/photo/:year
 // Displays: Image, Title, Full explanation, Back button
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 export default function PhotoDetail() {
-    return <h2>This is the Photo Detail page</h2>;
+    const {year} = useParams(); // Get year from URL
+
+    // React state hooks
+    const [photo, setPhoto] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState('');
+
+    // Navigation hook for back button
+    const navigate = useNavigate();
+
+    // Get DOB from localStorage
+    const dob = localStorage.getItem('dob');
+
+    useEffect(() => {
+        axios.get(`http://localhost:5000/api/nasa/photo/${year}?dob=${dob}`)
+            .then((res) => {
+                setPhoto(res.data);
+                setLoading(false);
+            })
+            .catch((err) => {
+                setError('Unable to load image details');
+                setLoading(false);
+            });
+    }, [year, dob]);
+
+    // Handle loading & error
+    if (!photo) return <h4>{error}</h4>;
+    if (loading) return <h4>Loading photo...</h4>;
+    if (error) return <h4>{error}</h4>;
+
+    return (
+        <div className="photodetail-container">
+            <button onClick={() => navigate('/timeline')} className="back-btn">
+                Back to timeline
+            </button>
+
+            <h2>{photo.title}</h2>
+            <p>{photo.date}</p>
+            <img src={photo.image} alt={photo.title} className="photo-full-info"/>
+            <p>{photo.description}</p>
+        </div>
+    )
 }
