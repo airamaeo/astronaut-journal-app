bd0529c7dcd1fa995b1f7983d1bbdd6f89bbb547
Aira Mae Ogaco <airamaeogaco@gmail.com>
2025-06-29 13:12:18 +0100

    Fix Vite base path and remove homepage for Vercel deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/astronaut-journal-app/',
+  // base: '/astronaut-journal-app/', // For Github pages - commenting out
+  base: './', // For Vercel deployment
   plugins: [react()],
   server: {
     open: true, // Tells Vite to open the browser automatically
